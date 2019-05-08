e96a3592537b14771d8181db39957b77bf34f8aa
nancylin <feizhou.lin@dasudian.com>
2019-05-08 13:18:16 +0800

    update config.py

--- config.py
+++ config.py
@@ -16,9 +16,12 @@ flags.DEFINE_float('lambda_val', 0.5, 'down weight of the loss for absent digit 
 flags.DEFINE_integer('batch_size', 128, 'batch size')
 flags.DEFINE_integer('epoch', 1, 'epoch')
 flags.DEFINE_integer('iter_routing', 3, 'number of iterations in routing algorithm')
-flags.DEFINE_boolean('mask_with_y', True, 'use the true label to mask out target capsule or not')
+flags.DEFINE_boolean('mask_with_y', True, '''use the true label to mask out target capsule or not''')
+
 flags.DEFINE_integer('train_data_number', 1280,'取多少数据进行训练')
 flags.DEFINE_integer('validation_data_number',256,'取多少数据进行校验')
+flags.DEFINE_integer('test_data_number', 1280, '取多少数据进行测试')
+
 
 flags.DEFINE_float('stddev', 0.01, 'stddev for W initializer')
 flags.DEFINE_float('regularization_scale', 0.392, 'regularization coefficient for reconstruction loss, default to 0.0005*784=0.392')
@@ -27,7 +30,7 @@ flags.DEFINE_float('regularization_scale', 0.392, 'regularization coefficient fo
 ############################
 #   environment setting    #
 ############################
-flags.DEFINE_string('dataset', 'mnist', 'The name of dataset [mnist, fashion-mnist')
+flags.DEFINE_string('dataset', 'mnist', 'The name of dataset [mnist, fashion-mnist]')
 flags.DEFINE_boolean('is_training', True, 'train or predict phase')
 flags.DEFINE_integer('num_threads', 8, 'number of threads of enqueueing examples')
 flags.DEFINE_string('logdir', 'logdir', 'logs directory')
